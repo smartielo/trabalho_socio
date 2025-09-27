e9f39467852b48ebc788c1baf83b029cbd998bd5
Emerson Mazzeto <em.mazzeto@gmail.com>
2025-09-26 23:28:33 -0300

    OI FAMILIA

--- frontend/trabalho_socio/src/App.js
+++ frontend/trabalho_socio/src/App.js
@@ -1,35 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Login from './pages/Login';
 
-function Home(){
-  const navigate = useNavigate();
-  function logout(){
-    localStorage.removeItem('mock_user');
-    navigate('/');
-  }
-  const user = JSON.parse(localStorage.getItem('mock_user') || 'null');
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Bem-vindo{user ? `, ${user.email}` : ''}!</h2>
-        <p>Esse é um exemplo de página inicial após login.</p>
-        <button onClick={logout}>Sair</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React GABRIIEL GABRIEL akdniodknawjdbnljkawbdhjkawbdhjavbjhdvbajhdgvawjgdhvajvked
+        </a>
       </header>
     </div>
-  )
-}
-
-function App(){
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
-  )
+  );
 }
 
 export default App;
