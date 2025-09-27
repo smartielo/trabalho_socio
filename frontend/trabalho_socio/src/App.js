f9910d94933d8bf3c6193332c2a4b62ff312451d
Gabriel Martielo <gabrielmartielo3214@gmail.com>
2025-09-26 23:14:01 -0300

    alteração

--- frontend/trabalho_socio/src/App.js
+++ frontend/trabalho_socio/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+//import About from './pages/About';
+//import Cadastro from './pages/Cadastro';
+//import Login from './pages/Login';
+//import Formulario from './pages/Formulario';
+//import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React GABRIIEL GABRIEL akdniodknawjdbnljkawbdhjkawbdhjavbjhdvbajhdgvawjgdhvajvked Elvio
-        </a>
-      </header>
-    </div>
+    // O BrowserRouter é o componente que "liga" o roteamento na sua aplicação.
+    // Ele deve envolver toda a lógica de rotas.
+    <BrowserRouter>
+      {/* Aqui dentro você pode colocar componentes que aparecem em todas as páginas, como Header e Footer */}
+      
+      {/* O componente Routes funciona como um "switch". Ele olha a URL atual
+          e renderiza a primeira rota <Route> que corresponder. */}
+      <Routes>
+        {/* Cada <Route> é uma regra. */}
+        {/* path="/" define a URL. */}
+        {/* element={...} define qual componente será renderizado para essa URL. */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/trabalho_socio/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <p>Bem-vindo ao site da nossa fundação!</p>
+    </div>
+  );
+};
+
+export default Home;
