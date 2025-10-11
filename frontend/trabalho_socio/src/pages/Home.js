08a28a7e9ea4e6522047279ed9c2a551d7dfcfb5
Emerson Mazzeto <em.mazzeto@gmail.com>
2025-10-10 21:02:57 -0300

    Alteração na página HOME

--- frontend/trabalho_socio/src/pages/Home.js
+++ frontend/trabalho_socio/src/pages/Home.js
@@ -7,27 +7,26 @@ import instituto from '../assets/instituto.png';
 import sagrado from '../assets/Sagrado.png';
 
 const Home = () => {
-  const handleLoginClick = () => {
-    alert('Botão de Login clicado!');
-  };
-
   const handleSignUpClick = () => {
     alert('Botão de Cadastrar clicado!');
   };
 
+  const handleLoginLinkClick = (e) => {
+    e.preventDefault(); // Impede o recarregamento da página
+    alert('Link "Entrar" clicado!');
+  };
+
   return (
     <div className="app-container">
-      <div className="content-wrapper">
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
-          <img src={instituto} alt="Instituto das Apostolas" style={{ width: '250px' }} />
-          <img src={sagrado} alt="Sagrado Coração de Jesus" style={{ width: '210px' }} />
-        </div>
-        <img src={brasao} alt="Brasão da Instituição" style={{ width: '300px', marginBottom: '30px' }} />
-        <div style={{ display: 'flex', gap: '20px' }}>
-          <Button onClick={handleLoginClick}>Entrar</Button>
-          <Button onClick={handleSignUpClick}>Cadastrar</Button>
-        </div>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+        <img src={instituto} alt="Instituto das Apostolas" style={{ width: '250px' }} />
+        <img src={sagrado} alt="Sagrado Coração de Jesus" style={{ width: '210px' }} />
       </div>
+      <img src={brasao} alt="Brasão da Instituição" style={{ width: '300px', marginBottom: '30px' }} />
+      <Button onClick={handleSignUpClick}>Cadastrar</Button>
+      <p className="login-link">
+        Já possui um cadastro? <a href="#" onClick={handleLoginLinkClick}>Entrar</a>
+      </p>
     </div>
   );
 };
